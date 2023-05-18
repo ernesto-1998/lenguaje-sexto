a9f7593bd0a2c1a4306bd0f596ad92127ef891d4
ernesto-1998 <ernestoalonso1998@gmail.com>
2023-05-18 13:05:45 -0600

    Primer commit

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import "./header.css"
+
+export function Header() {
+    return (
+        <header>
+            <div className="title-container">
+                <h1>Material Multimedia Lenguaje 6° Grado</h1>
+            </div>
+            <nav>
+                <ul className="nav-menu">
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+}
