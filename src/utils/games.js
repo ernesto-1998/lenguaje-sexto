34297618497f3a1c4301072fa2ada0306b88c34d
ernesto-1998 <ernestoalonso1998@gmail.com>
2023-05-20 14:27:07 -0600

    Cambiando directorio de imagenes, de assets a public

--- src/utils/games.js
+++ src/utils/games.js
@@ -1,16 +1,16 @@
 export const games1 = [
     {
-        img: "src/assets/games1/verdaderoFalso.png",
+        img: "public/games1/verdaderoFalso.png",
         title: "Falso o Verdadero | El cuento de terror",
         link: "https://wordwall.net/es/resource/56411341"
     },
     {
-        img: "src/assets/games1/sopaLetras.png",
+        img: "public/games1/sopaLetras.png",
         title: "Sopa de Letras | La historieta",
         link: "https://wordwall.net/es/resource/56563952"
     },
     {
-        img: "src/assets/games1/opcionMultiple.png",
+        img: "public/games1/opcionMultiple.png",
         title: "Opción Múltiple | Prefijos y Sufijos",
         link: "https://wordwall.net/es/resource/56563779"
     },
